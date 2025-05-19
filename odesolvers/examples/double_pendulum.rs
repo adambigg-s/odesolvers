049b2859dc361d906d65bec0fee5062bddacc769
adambigg-s <biggsa601@gmail.com>
2025-05-19 14:17:11 -0400

    added linear damping

--- odesolvers/examples/double_pendulum.rs
+++ odesolvers/examples/double_pendulum.rs
@@ -12,17 +12,17 @@ const PLOT_HEIGHT: usize = 70;
 fn main() {
     let dt = 0.1;
     let final_time = 10000.;
-    let initial_state: [f32; 4] = [PI * 9. / 11., PI * 9. / 11., 0., 0.];
+    let initial_state: [f32; 4] = [PI * 10. / 11., PI * 10. / 11., 0., 0.];
     let mut integrator = Integrator::build(initial_state, dt, double_pendulum_dynamics);
 
-    let mut states = StateTracker::build(50);
+    let mut states = StateTracker::build(30);
     let mut plot = Plot::build(PLOT_HEIGHT, PLOT_WIDTH);
     plot.xbounds(-30., 30.)
         .ybounds(-22., 22.)
         .set_settings()
         .subtick(true)
         .subtick_spacing(5.)
-        .axis_color(30, 30, 30);
+        .axis_color(90, 30, 30);
 
     while integrator.curr_time() < final_time {
         integrator.step();
@@ -59,10 +59,12 @@ fn main() {
 }
 
 const M1: f32 = 3.;
-const M2: f32 = 1.5;
-const L1: f32 = 13.;
-const L2: f32 = 7.;
+const M2: f32 = 2.5;
+const L1: f32 = 12.;
+const L2: f32 = 8.;
 const G: f32 = 9.81;
+const C1: f32 = 0.001;
+const C2: f32 = 0.01;
 
 fn double_pendulum_dynamics(state: &[f32; 4]) -> [f32; 4] {
     let [t1, t2, w1, w2] = *state;
@@ -73,14 +75,18 @@ fn double_pendulum_dynamics(state: &[f32; 4]) -> [f32; 4] {
     let den_partial = 2. * M1 + M2 - M2 * (2. * t1 - 2. * t2).cos();
 
     let num1 = -G * (2. * M1 + M2) * t1.sin()
-        - M2 * G * (t1 - 2. * t1).sin()
+        - M2 * G * (t1 - 2. * t2).sin()
         - 2. * (t1 - t2).sin() * M2 * (w2 * w2 * L2 + w1 * w1 * L1 * (t1 - t2).cos());
-    let w1ddt = num1 / (L1 * den_partial);
+    let mut w1ddt = num1 / (L1 * den_partial);
+    // linear dampening model
+    w1ddt -= C1 * w1;
 
     let num2 = 2.
         * (t1 - t2).sin()
         * (w1 * w1 * L1 * (M1 + M2) + G * (M1 + M2) * t1.cos() + w2 * w2 * L2 * M2 * (t1 - t2).cos());
-    let w2ddt = num2 / (L2 * den_partial);
+    let mut w2ddt = num2 / (L2 * den_partial);
+    // linear dampening model
+    w2ddt -= C2 * w2;
 
     [t1ddt, t2ddt, w1ddt, w2ddt]
 }
